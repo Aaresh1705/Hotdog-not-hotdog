8215f5ce577c40b168cbc510ced8c939405e8a0b
Marcus <marcus.olssen@gmail.com>
2025-09-19 11:49:39 +0200

    feat: Changed the get_vgg16_model function to look more clean and understandable

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torchvision.models import vgg16
 from torchvision.models import VGG16_Weights
 
+
 def get_vgg16_model(pretrained: bool=False, custom_weights: str='') -> nn.Module:
     """
     Returns the VGG16 model
@@ -11,23 +12,34 @@ def get_vgg16_model(pretrained: bool=False, custom_weights: str='') -> nn.Module
     :return: The VGG16 model
     """
 
+    def get_model(weights) -> nn.Module:
+        model = vgg16(pretrained=weights)
+        # print(model.classifier)
+        model.classifier[6] = nn.Linear(in_features=4096, out_features=2)
+
+        for param in model.classifier.parameters():
+            param.requires_grad = True
+
+        return model
+
     if pretrained:
-        model = vgg16(weights=VGG16_Weights)
-        model.classifier[6] = nn.Linear(in_features=4096, out_features=2)
+        model = get_model(weights=VGG16_Weights)
+
+        for param in model.features.parameters():
+            param.requires_grad = False
+
+        return model
+
     else:
-        model = vgg16(weights=None)
-        model.classifier[6] = nn.Linear(in_features=4096, out_features=2)
+        model = get_model(weights=None)
         if custom_weights != '':
             model.load_state_dict(torch.load(custom_weights, weights_only=True))
 
-    # print(model.classifier)
+        for param in model.features.parameters():
+            param.requires_grad = True
 
-    for param in model.features.parameters():
-        param.requires_grad = False
-    for param in model.classifier.parameters():
-        param.requires_grad = True
+        return model
 
-    return model
 
 def get_custom_model():
     class Network(nn.Module):
